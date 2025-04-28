52f1aaf750d270110451822a2bfeee543f8575ca
Sangyoon <qlrqod123123@naver.com>
2025-04-28 21:36:11 +0900

    최소 힙

---
+++ src/baekjoon/dataStructure/B1927.java
@@ -0,0 +1,24 @@
+package baekjoon.dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+// 최소 힙
+public class B1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x != 0) queue.add(x);
+            else {
+                if (queue.isEmpty()) System.out.println("0");
+                else System.out.println(queue.poll());
+            }
+        }
+    }
+}
