e38c737edc16b4bc814cfb04411447c9027b63db
Sangyoon <qlrqod123123@naver.com>
2025-04-29 08:44:44 +0900

    절댓값 힙

---
+++ src/baekjoon/dataStructure/B11286.java
@@ -0,0 +1,33 @@
+package baekjoon.dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class B11286 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) == Math.abs(o2)) {
+                    if (o1 < o2) return -1;
+                    else return 1;
+                } else return Math.abs(o1) - Math.abs(o2);
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x != 0) queue.add(x);
+            else {
+                if (queue.isEmpty()) System.out.println("0");
+                else  System.out.println(queue.poll());
+            }
+        }
+    }
+}
