bbf41d2a6bfe0e4ed345a1e928b912ec902c9ced
Sangyoon <qlrqod123123@naver.com>
2025-04-29 23:10:51 +0900

    쇠막대기

---
+++ src/baekjoon/dataStructure/B10799.java
@@ -0,0 +1,27 @@
+package baekjoon.dataStructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// 쇠막대기
+public class B10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().replace("()", "R").split("");
+        Stack<String> stack = new Stack<>();
+
+        int total = 0;
+
+        for (String s : input) {
+            if (s.equals("(")) stack.push("(");
+            else if (s.equals(")")) {
+                stack.pop();
+                total++;
+            } else total += stack.size();
+        }
+
+        System.out.print(total);
+    }
+}
